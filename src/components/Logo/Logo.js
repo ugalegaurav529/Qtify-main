b25d0d215a027c161ab17f220eff0fe9c0180540
Gaurav Ugale <143158225+ugalegaurav529@users.noreply.github.com>
2024-07-21 00:04:24 +0530

    commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,17 @@
 import logo from "../../assets/Logo.png";
 
 const Logo = () => {
-  return <img src={logo} alt="logo" />;
+  return (
+    <img
+      src={logo}
+      alt="logo"
+      style={{
+        verticalAlign: "middle",
+        marginLeft: 20, /* adjust the value to your liking */
+        marginRight: 20 /* adjust the value to your liking */
+      }}
+    />
+  );
 };
 
 export default Logo;
