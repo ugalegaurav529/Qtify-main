dfce9c10d3039aef4bbe21378f61b92c18be1ec4
Gaurav Ugale <143158225+ugalegaurav529@users.noreply.github.com>
2024-07-21 09:07:00 +0530

    commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,13 @@
-import logo from "../../assets/Logo.png";
-import "./Logo.css";
+import React from 'react';
+import logo from "../../assets/Logo.png"; // Importing the logo image
+import "./Logo.css"; // Importing the CSS file for styling
+
 const Logo = () => {
-  return <img src={logo} alt="logo" className="logo"/>;
+    return (
+        <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
+        </div>
+    );
 };
 
 export default Logo;
